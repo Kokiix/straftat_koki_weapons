23cda542a2e4b50d9be4049691cdbdc4fde38053
kenneth <kokiidoki@gmail.com>
2026-05-07 11:11:43 -0400

    init

---
+++ KokiWeapons/Plugin.cs
@@ -0,0 +1,26 @@
+using BepInEx;
+using BepInEx.Logging;
+using ComputerysModdingUtilities;
+using HarmonyLib;
+
+[assembly: StraftatMod(isVanillaCompatible: true)]
+
+namespace KokiWeapons;
+
+[BepInPlugin("com.koki.weapons", "Koki Weapons", "1.0.0")]
+public class KokiWeaponsPlugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    internal static Harmony harmony;
+    private void Awake()
+    {
+        Logger = base.Logger;
+        harmony = new Harmony("com.koki.weapons");
+        harmony.PatchAll();
+    }
+
+    private void OnDestroy()
+    {
+        harmony.UnpatchSelf();
+    }
+}
