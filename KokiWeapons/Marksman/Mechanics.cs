afe145f770c966de30390e32aab2b399b7f9d5ba
kenneth <kokiidoki@gmail.com>
2026-05-12 20:39:58 -0400

    throw coin input

--- KokiWeapons/Marksman/Mechanics.cs
+++ KokiWeapons/Marksman/Mechanics.cs
@@ -7,5 +7,20 @@ using UnityEngine;
 [HarmonyPatch]
 public static class MarksmanMechanics
 {
+    [HarmonyPatch(typeof(FirstPersonController), "SetZoom")]
+    [HarmonyPatch(typeof(FirstPersonController), "SetZoomToggle")]
+    [HarmonyPostfix]
+    public static void TriggerCoinThrowToggle(FirstPersonController __instance)
+    {
+        if (__instance.playerPickupScript.behaviourInHand.name == Marksman.name + "(Clone)")
+        {
+            if (__instance.isZooming) ThrowCoin();
+            __instance.isZooming = false;
+        }
+    }
 
+    public static void ThrowCoin()
+    {
+        KokiDebug.Log("throw");
+    }
 }
