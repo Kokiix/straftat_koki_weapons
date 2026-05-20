20b2d311df4f0574a40746f8fe4945169238dd58
kenneth <kokiidoki@gmail.com>
2026-05-19 21:50:21 -0400

    car moving forward with partial camera change

--- KokiWeapons/Components/RCCar/RCCar.cs
+++ KokiWeapons/Components/RCCar/RCCar.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class RCCar : MonoBehaviour
 {
     [SerializeField]
+    private Transform _cameraPosition;
+    [SerializeField]
     private Rigidbody _rb;
+    [SerializeField]
+    private float _accel;
+    [SerializeField]
+    private float _turnSpeed;
 
+    private FirstPersonController _driver;
     private UnityEngine.InputSystem.InputAction _moveInput;
     private bool _driving = false;
 
@@ -23,18 +30,42 @@ public class RCCar : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        if (_driving)
+            EndDriving();
+    }
+
+    private void Update()
+    {
+        if (!_driving) return;
+        var inputVector = _moveInput.ReadValue<Vector2>();
+        if (inputVector.x != 0)
+            transform.Rotate(0, inputVector.x * _turnSpeed * Time.deltaTime, 0);
+    }
+
     private void FixedUpdate()
     {
         if (!_driving) return;
         var inputVector = _moveInput.ReadValue<Vector2>();
-        Debug.LogError(inputVector.y);
-        // if (_moveInput.)
+        if (inputVector.y != 0)
+            _rb.AddForce(transform.forward * _accel * inputVector.y);
     }
 
     public void BeginDriving(FirstPersonController driver)
     {
         _moveInput = driver.move;
         _driving = true;
+        _driver = driver;
         driver.sync___set_value_canMove(false, true);
+
+        driver.playerCamera.transform.SetParent(_cameraPosition);
+    }
+
+    public void EndDriving()
+    {
+        _driving = false;
+        _driver.sync___set_value_canMove(true, true);
+        _driver.playerCamera.transform.SetParent(_driver.playerCameraHolder.transform);
     }
 }
